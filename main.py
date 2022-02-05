c71d40d58684cc2fb77be4b73aba82270f4596d3
henryfbp <HenryFBP@gmail.com>
2022-02-04 21:55:56 -0500

    fininsh juicing da world

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 # Press Ctrl+F5 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+from typing import List, Tuple
+
+DEFAULT_SIZE = (15, 20)
 
 
 def gen_tile() -> str:
@@ -13,12 +17,46 @@ def gen_tile() -> str:
     return ' '
 
 
-world = []
+def gen_world(size=DEFAULT_SIZE) -> List[List[str]]:
+    width, height = size
+    resultworld = []
+    for y in range(0, height):
+        row = []
+        for x in range(0, width):
+            row.append(gen_tile())
+        resultworld.append(row)
+    return resultworld
 
-for y in range(0, 10):
-    row = []
-    for x in range(0, 30):
-        row.append(' ')
+
+def print_world(juiceWorld: List[List[str]], size: Tuple[int, int] = DEFAULT_SIZE, borderchar: str = None) -> None:
+    width, height = size
+    for y in range(0, height):
+        row = juiceWorld[y]
+
+        if (y == 0) and borderchar:
+            print((borderchar * 2) + (borderchar * width))
+
+        for x in range(0, width):
+            tile = row[x]
+
+            if (x == 0) and borderchar:
+                print(borderchar, end='')
+
+            print(tile, end='')
+
+            if (x == (width - 1)) and borderchar:
+                print(borderchar, end='')
+
+        print("\n", end='')
+
+        if (y == (height - 1)) and borderchar:
+            print((borderchar * 2) + (borderchar * width))
+
+    return None
+
+
+world = gen_world()
+print_world(world, size=DEFAULT_SIZE, borderchar='#')
 
 
 def print_hi(name):
