61b63dd9b188d0ac92a5fc3c283c893ad2f3ea13
Henry Post <HenryFBP@gmail.com>
2022-02-09 10:19:14 -0500

    use python3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ echo "Make sure pipenv exists..."
 which pipenv
 # if exit code is nonzero, it is not a command.
 if [ "$?" -eq "1" ]; then
-  python -m pip install pipenv
+  python3 -m pip install pipenv
 fi
 
 pipenv install
