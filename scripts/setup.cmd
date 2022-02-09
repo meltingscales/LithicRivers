0e061858652a88160822f5a6982bb637689b371e
henryfbp <HenryFBP@gmail.com>
2022-02-08 22:55:11 -0500

    use --dev

--- scripts/setup.cmd
+++ scripts/setup.cmd
@@ -2,4 +2,4 @@ echo "Make sure pipenv exists..."
 WHERE poetry
 IF %ERRORLEVEL% NEQ 0 python -m pip install pipenv
 
-python3 -m pipenv install
+python3 -m pipenv install --dev
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,9 +8,6 @@ if [ "$?" -eq "1" ]; then
   python -m pip install pipenv
 fi
 
-echo "Making sure required tools are installed."
-sudo apt-get install -y xdotool wmctrl
-
 pipenv install
 
-python3 -m pipenv install
+python3 -m pipenv install --dev
