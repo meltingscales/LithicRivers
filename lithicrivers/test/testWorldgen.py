0062ebc53c92c315e3ad0a1b8d62bac4070b7d83
henryfbp <HenryFBP@gmail.com>
2022-02-11 21:56:42 -0500

    complex worldgen

---
+++ lithicrivers/test/testWorldgen.py
@@ -0,0 +1,32 @@
+import unittest
+
+from lithicrivers.game import World, Tiles
+from lithicrivers.model import Vector2
+
+
+class RenderStuff(unittest.TestCase):
+    def testworldGen(self):
+        for _ in range(0, 100):
+            self.assertEquals(
+                World.gen_random_world_data(
+                    Vector2(3, 3),
+                    gen_function=lambda: Tiles.DaFuq()
+                ),
+                [
+                    [
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq()
+                    ],
+                    [
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq()
+                    ],
+                    [
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq(),
+                        Tiles.DaFuq()
+                    ]
+                ]
+            )
