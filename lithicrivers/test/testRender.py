ba48599d69dc3343c546c4d54418f922467d7185
henryfbp <HenryFBP@gmail.com>
2022-02-11 21:56:42 -0500

    complex worldgen

--- lithicrivers/test/testRender.py
+++ lithicrivers/test/testRender.py
@@ -10,10 +10,3 @@ class RenderStuff(unittest.TestCase):
         self.assertEquals(Tiles.DaFuq().render_sprite(1), "?")
         self.assertEquals(Tiles.DaFuq().render_sprite(2), "??\n??")
 
-    def testworldRendering(self):
-        for _ in range(0, 100):
-            self.assertEquals(
-                World.gen_random_world_data(Vector2(1, 1),
-                                            gen_function=lambda: Tiles.DaFuq())[0][0],
-                Tiles.DaFuq()
-            )
