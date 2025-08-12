2ac5c47891671e4fd50bf219e88e840404bc1049
Henry Post <HenryFBP@gmail.com>
2025-08-11 19:03:21 -0500

    speedup linking

--- shell.nix
+++ shell.nix
@@ -19,9 +19,10 @@ pkgs.mkShell {
     pkgs.udev
     pkgs.vulkan-loader
     pkgs.mesa
+    pkgs.mold
   ];
   shellHook = ''
     export PKG_CONFIG_PATH=${pkgs.alsa-lib.dev}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
-    export LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXrender}/lib:${pkgs.xorg.libXi}/lib:${pkgs.vulkan-loader}/lib:${pkgs.mesa.drivers}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXrender}/lib:${pkgs.xorg.libXi}/lib:${pkgs.vulkan-loader}/lib:${pkgs.mesa}/lib:${pkgs.udev}/lib:${pkgs.alsa-lib}/lib:$LD_LIBRARY_PATH
   '';
 }
