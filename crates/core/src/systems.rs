41898b18c76e82e13af78b1dae36a039cdb6f741
Henry Post <HenryFBP@gmail.com>
2025-08-11 22:28:14 -0500

    simplify wall/floor status

--- crates/core/src/systems.rs
+++ crates/core/src/systems.rs
@@ -1,6 +1,8 @@
 use crate::components::Position;
 use crate::resources::Resources;
+use crate::tiles::TileKind;
 use hecs::World;
+use tracing::info;
 
 pub fn move_player_system(world: &mut World, res: &mut Resources) {
     if let Some((dx, dy)) = res.player_move_intent.take() {
@@ -8,12 +10,12 @@ pub fn move_player_system(world: &mut World, res: &mut Resources) {
             if let Ok(mut pos) = world.get::<&mut Position>(player_e) {
                 let nx = pos.x + dx;
                 let ny = pos.y + dy;
-                // Collision check against walls in infinite, lazy-generated world
-                use crate::resources::world::Tile;
                 let t = res.world.get_tile(nx, ny);
-                if t != Tile::Wall {
+                if t.is_passable() {
                     pos.x = nx;
                     pos.y = ny;
+                } else {
+                    info!("Blocked by {:?} at ({}, {})", t, nx, ny);
                 }
             }
         }
