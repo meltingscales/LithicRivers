7644759328cef8f2c0654d89b173424ede3281f5
Henry Post <HenryFBP@gmail.com>
2025-09-02 15:59:16 -0500

    split crafting panel

--- crates/client/src/ui/panels/mod.rs
+++ crates/client/src/ui/panels/mod.rs
@@ -1,8 +1,10 @@
+pub mod crafting;
 pub mod credits;
 pub mod help;
 pub mod inventory;
 pub mod quit;
 
+pub use crafting::render_crafting_panel;
 pub use credits::render_credits_panel;
 pub use help::render_help_panel;
 pub use inventory::{get_player_inventory, render_inventory_list_only, render_inventory_panel};
