beedc37c61f0fb09e28487e1b79cf2bb9b7a2f76
Henry Post <HenryFBP@gmail.com>
2025-09-02 15:56:42 -0500

    split inventory panel

--- crates/client/src/ui/panels/mod.rs
+++ crates/client/src/ui/panels/mod.rs
@@ -1,7 +1,9 @@
 pub mod credits;
 pub mod help;
+pub mod inventory;
 pub mod quit;
 
 pub use credits::render_credits_panel;
 pub use help::render_help_panel;
+pub use inventory::{get_player_inventory, render_inventory_list_only, render_inventory_panel};
 pub use quit::render_quit_panel;
