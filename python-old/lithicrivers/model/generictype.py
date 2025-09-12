be07c3d546961493dd1845f12087bf1e2839a835
Henry Post <HenryFBP@gmail.com>
2025-09-12 13:34:13 -0500

    delete old python code

--- python-old/lithicrivers/model/generictype.py
+++
@@ -1,9 +0,0 @@
-"""
-Generic type definitions for LithicRivers
-Copyright (c) 2024 Henry Post. All rights reserved.
-"""
-
-from typing import TypeVar
-
-# Generic type parameter for weighted choice functions
-T = TypeVar("T")
