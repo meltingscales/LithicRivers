06c2b1ef14d0877370e1e401612f0cf4ab2f4fad
Henry Post <HenryFBP@gmail.com>
2025-09-16 14:25:06 -0500

    stub for sprite demo

---
+++ crates/client/src/bin/utility_sprite_demo.rs
@@ -0,0 +1,7 @@
+// todo...
+// a demo that loads every tile, item, entity, and structure
+// and lets you cycle through them. it should display their colors, 1x1/2x2/3x3, and 12x8 art.
+// for entities, it should also display thier emotions.
+
+// for structures, it should also display their names and descriptions, their spawn chances,
+// and let you cycle up/down/left/right through them, as well as zoom in or out.
