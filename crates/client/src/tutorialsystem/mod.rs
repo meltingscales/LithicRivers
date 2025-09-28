598174353fa1968c26ca842469392d4d9e1c89eb
Henry Post <HenryFBP@gmail.com>
2025-09-28 15:16:04 -0500

    extract tutorial steps to tutorial_database.rs

--- crates/client/src/tutorialsystem/mod.rs
+++ crates/client/src/tutorialsystem/mod.rs
@@ -1,7 +1,9 @@
+pub mod tutorial_database;
 pub mod tutorial_overlay;
 pub mod tutorial_step;
 pub mod tutorial_system;
 
+pub use tutorial_database::TutorialDatabase;
 #[allow(unused_imports)]
 pub use tutorial_step::{TutorialAction, TutorialStep};
 pub use tutorial_system::TutorialSystem;
